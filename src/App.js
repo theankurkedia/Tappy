8be13611e6a41816fc9e059c2b5cf790fde511df
Ankur Kedia <ankur.kedia@paytm.com>
2021-05-28 16:22:19 +0530

    kicked off project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,89 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+const COLORS = {
+  Red: '#e11d48',
+  Blue: '#2563eb',
+};
 
 function App() {
+  const [values, setValues] = React.useState({ red: 50, blue: 50 });
+  const [result, setResult] = React.useState('Red');
+
+  const handlePress = (color) => {
+    if (color === 'Red') {
+      if (values.blue >= 2) {
+        setValues({ red: values.red + 2, blue: values.blue - 2 });
+      } else {
+        setValues({ red: 50, blue: 50 });
+        setResult('Red');
+      }
+    } else {
+      if (values.red >= 2) {
+        setValues({ red: values.red - 2, blue: values.blue + 2 });
+      } else {
+        setValues({ red: 50, blue: 50 });
+        setResult('Blue');
+      }
+    }
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div style={{ position: 'absolute', height: '100vh', width: '100vw' }}>
+      {result ? (
+        <div
+          style={{
+            backgroundColor: COLORS[result === 'Red' ? 'Red' : 'Blue'],
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'center',
+            alignItems: 'center',
+            height: '100%',
+            width: '100%',
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          <h6
+            style={{
+              color: 'white',
+              fontSize: '1.5rem',
+              margin: 10,
+              pointerEvents: 'none',
+            }}
+          >{`${result} wins`}</h6>
+          <button
+            onClick={() => setResult(null)}
+            className='button'
+            style={{
+              fontSize: '1rem',
+              padding: 10,
+              backgroundColor: COLORS[result === 'Red' ? 'Blue' : 'Red'],
+              width: '100',
+              color: 'white',
+              borderWidth: 0,
+              fontWeight: 700,
+              borderRadius: 10,
+              height: 40,
+            }}
+          >
+            Play again?
+          </button>
+        </div>
+      ) : (
+        <>
+          <div
+            style={{
+              backgroundColor: COLORS.Red,
+              height: values.red + '%',
+            }}
+            onClick={() => handlePress('Red')}
+          />
+          <div
+            style={{
+              backgroundColor: COLORS.Blue,
+              height: values.blue + '%',
+            }}
+            onClick={() => handlePress('Blue')}
+          />
+        </>
+      )}
     </div>
   );
 }
